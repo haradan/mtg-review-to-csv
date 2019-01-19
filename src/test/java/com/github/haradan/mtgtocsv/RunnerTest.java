0ffd5c2f952fed9097a2b5d0b405d79c42c4103a
Haradan <haradan@users.noreply.github.com>
2019-01-19 17:04:56 +0000

    Add more sets, make separate class for running without testing

---
+++ src/test/java/com/github/haradan/mtgtocsv/RunnerTest.java
@@ -0,0 +1,139 @@
+package com.github.haradan.mtgtocsv;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.File;
+
+@Ignore
+public class RunnerTest {
+
+	@Test
+	public void canConvertGuildsOfRavnicaLtdToCSV() throws Exception {
+
+		LSVSetReview review = LSVSetReview.fromColourURLs(
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-white/",
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-blue/",
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-black/",
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-red/",
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-green/",
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-boros/",
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-dimir/",
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-golgari/",
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-izzet/",
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-selesnya/",
+				"https://www.channelfireball.com/articles/guilds-of-ravnica-limited-set-review-artifacts-lands-and-guild-ranking/");
+
+		File file = new File("test/guilds-of-ravnica-ltd.csv");
+		review.outputToCSV(file);
+	}
+
+	@Test
+	public void canConvertDominariaLtdToCSV() throws Exception {
+
+		LSVSetReview review = LSVSetReview.fromColourURLs(
+				"https://www.channelfireball.com/articles/dominaria-limited-set-review-white/",
+				"https://www.channelfireball.com/articles/dominaria-limited-set-review-blue/",
+				"https://www.channelfireball.com/articles/dominaria-limited-set-review-black/",
+				"https://www.channelfireball.com/articles/dominaria-limited-set-review-red/",
+				"https://www.channelfireball.com/articles/dominaria-limited-set-review-green/",
+				"https://www.channelfireball.com/articles/dominaria-limited-set-review-gold-artifacts-and-lands/");
+
+		File file = new File("test/dominaria-ltd.csv");
+		review.outputToCSV(file);
+	}
+
+	@Test
+	public void canConvertDominariaConstructedToCSV() throws Exception {
+
+		LSVSetReview review = LSVSetReview.fromColourURLs(
+				"https://www.channelfireball.com/articles/dominaria-constructed-set-review-white/",
+				"https://www.channelfireball.com/articles/dominaria-constructed-set-review-blue/",
+				"https://www.channelfireball.com/articles/dominaria-constructed-set-review-black/",
+				"https://www.channelfireball.com/articles/dominaria-constructed-set-review-red/",
+				"https://www.channelfireball.com/articles/dominaria-constructed-set-review-green/",
+				"https://www.channelfireball.com/articles/dominaria-constructed-set-review-gold-artifacts-and-lands/");
+
+		File file = new File("test/dominaria-constructed.csv");
+		review.outputToCSV(file);
+	}
+
+	@Test
+	public void canConvertRIXLtdToCSV() throws Exception {
+
+		LSVSetReview review = LSVSetReview.fromColourURLs(
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-limited-set-review-white/",
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-limited-set-review-blue/",
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-limited-set-review-black/",
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-limited-set-review-red/",
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-limited-set-review-green/",
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-limited-set-review-gold-artifacts-and-lands/");
+
+		File file = new File("test/rix-ltd.csv");
+		review.outputToCSV(file);
+	}
+
+	@Test
+	public void canConvertRIXConstructedToCSV() throws Exception {
+
+		LSVSetReview review = LSVSetReview.fromColourURLs(
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-constructed-set-review-white/",
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-constructed-set-review-blue/",
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-constructed-set-review-black/",
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-constructed-set-review-red/",
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-constructed-set-review-green/",
+				"https://www.channelfireball.com/articles/rivals-of-ixalan-constructed-set-review-gold-artifacts-and-lands/");
+
+		File file = new File("test/rix-constructed.csv");
+		review.outputToCSV(file);
+	}
+
+	@Test
+	public void canConvertIxalanLtdToCSV() throws Exception {
+
+		LSVSetReview review = LSVSetReview.fromColourURLs(
+				"https://www.channelfireball.com/articles/ixalan-limited-set-review-white/",
+				"https://www.channelfireball.com/articles/ixalan-limited-set-review-blue/",
+				"https://www.channelfireball.com/articles/ixalan-limited-set-review-black/",
+				"https://www.channelfireball.com/articles/ixalan-limited-set-review-red/",
+				"https://www.channelfireball.com/articles/ixalan-limited-set-review-green/",
+				"https://www.channelfireball.com/articles/ixalan-limited-set-review-gold-artifacts-and-lands/");
+
+		File file = new File("test/ixalan-ltd.csv");
+		review.outputToCSV(file);
+	}
+
+	@Test
+	public void canConvertIxalanConstructedToCSV() throws Exception {
+
+		LSVSetReview review = LSVSetReview.fromColourURLs(
+				"https://www.channelfireball.com/articles/ixalan-constructed-set-review-white/",
+				"https://www.channelfireball.com/articles/ixalan-constructed-set-review-blue/",
+				"https://www.channelfireball.com/articles/ixalan-constructed-set-review-black/",
+				"https://www.channelfireball.com/articles/ixalan-constructed-set-review-red/",
+				"https://www.channelfireball.com/articles/ixalan-constructed-set-review-green/",
+				"https://www.channelfireball.com/articles/ixalan-constructed-set-review-gold-artifacts-and-lands/");
+
+		File file = new File("test/ixalan-constructed.csv");
+		review.outputToCSV(file);
+	}
+
+	@Test
+	public void canConvertRavnicaAllegianceLtdToCSV() throws Exception {
+
+		LSVSetReview review = LSVSetReview.fromColourURLs(
+				"https://www.channelfireball.com/articles/ravnica-allegiance-limited-set-review-white/",
+				"https://www.channelfireball.com/articles/ravnica-allegiance-limited-set-review-blue/",
+				"https://www.channelfireball.com/articles/ravnica-allegiance-limited-set-review-black/",
+				"https://www.channelfireball.com/articles/ravnica-allegiance-limited-set-review-red/",
+				"https://www.channelfireball.com/articles/ravnica-allegiance-limited-set-review-green/",
+				"https://www.channelfireball.com/articles/ravnica-allegiance-limited-set-review-azorius/",
+				"https://www.channelfireball.com/articles/ravnica-allegiance-limited-set-review-gruul/",
+				"https://www.channelfireball.com/articles/ravnica-allegiance-limited-set-review-orzhov/",
+				"https://www.channelfireball.com/articles/ravnica-allegiance-limited-set-review-simic-and-colorless/",
+				"https://www.channelfireball.com/articles/ravnica-allegiance-limited-set-review-rakdos/");
+
+		File file = new File("test/ravnica-allegiance-ltd.csv");
+		review.outputToCSV(file);
+	}
+}
